6237f8c8ece06ce5cf675df7e71494f0dd96b412
ElCart3 <elliejane.carter@gmail.com>
2022-03-12 17:08:33 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,5 +3,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <footer>
+          This project is coded by Ellie Carter and is{" "}
+          <a
+            href="https://github.com/ElCart3/react-weather-app"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
